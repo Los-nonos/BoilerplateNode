abb65fd411041a86341e8ffae06eb9d062275b11
cristianvena18 <cristiandamianvena@gmail.com>
2021-09-24 23:59:20 -0300

    fix: runned prettier

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
-import convict from 'convict';
+import convict from "convict";
 
-const config = convict({
-  
-});
+const config = convict({});
 
-config.loadFile([__dirname + '/default.json', __dirname + '/' + config.get('env') + '.json']);
+config.loadFile([
+  __dirname + "/default.json",
+  __dirname + "/" + config.get("env") + ".json",
+]);
 
 export default config;
